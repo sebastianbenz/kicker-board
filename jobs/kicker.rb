75518adba332018da76efd5a6689db15e33e6897
Sebastian Benz <sebastian.benz@gmail.com>
2013-11-19 23:56:30 +0100

    kicker job

--- jobs/kicker.rb
+++ jobs/kicker.rb
@@ -1,14 +1,18 @@
 # :first_in sets how long it takes before the job is first run. In this case, it is run immediately
 require 'redis'
 
-Thread.start do
-  redis = Redis.new
-  redis.subscribe("events") do |on|
-    puts "waiting for events"
-    on.message do |channel, msg|
-      send_event('my_widget', { text: msg})
-      puts "##{channel} - #{msg}"
+def wait_for_player(redis, color, position)
+  Thread.start do
+    redis.subscribe("kicker:register:player:#{color}:#{position}") do |on|
+      on.message do |channel, msg|
+        puts "#{{ "player_#{color}_#{position}.name" => msg}}"
+        send_event("kicker", { "player_#{color}_#{position}.name" => msg})
+        send_event("testid", { value: msg })
+        puts "##{channel} - #{msg}"
+      end
     end
   end
-  puts "stopped for events"
 end
+
+redis = Redis.new
+wait_for_player(redis, "black", "back")
