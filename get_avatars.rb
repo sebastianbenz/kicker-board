20f282b5a0ee22e1883941d9cee7c8d0fee5b386
Christian Köstlin <christian.koestlin@esrlabs.com>
2013-11-20 17:16:37 +0100

    more avatars

--- get_avatars.rb
+++ get_avatars.rb
@@ -5,13 +5,15 @@ users = [
          'christian.koestlin@esrlabs.com',
          'gerd.schaefer@esrlabs.com',
          'daniel.schick@esrlabs.com',
+         'daniel.schick@gmail.com',
          'matthias.kessler@esrlabs.com',
          'oliver.mueller@esrlabs.com'
          ]
 users.each do |u|
-  file_name = "#assets/images/avatars/{u}.png"
+  file_name = "assets/images/avatars/#{u}.png"
+  puts "Working for #{u}"
   if !File.exists?(file_name)
-    data = Gravatar.new(u).image_data
+    data = Gravatar.new(u).image_data({:size => 512})
     File.open(file_name, 'wb') do |io|
       io.write(data)
     end
