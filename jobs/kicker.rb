519ef806cd9e687a06e42b797b9e22e0a28877ea
Sebastian Benz <sebastian.benz@gmail.com>
2013-11-20 11:56:57 +0100

    track game score

--- jobs/kicker.rb
+++ jobs/kicker.rb
@@ -1,5 +1,6 @@
 # :first_in sets how long it takes before the job is first run. In this case, it is run immediately
 require 'redis'
+require 'json'
 
 def avatar(email_address)
   return avatar_url(email_address) if File.exists?(avatar_path(email_address))
@@ -14,23 +15,39 @@ def avatar_url(email_address)
   "/assets/avatars/#{email_address}.png"
 end
 
+def wait_for_score
+  subscribe("kicker:game:score") do |channel, msg|
+    score = JSON.parse(msg)
+    score.each do |key, value|
+      puts "#{key} -> #{value}"
+    #send_event("kicker-player-#{color}-#{position}", { player: player })
+    end
+  end
+end
+
 def wait_for_players
+  subscribe("kicker:register:*") do |channel, msg|
+    fragments = channel.split(":")
+    color = fragments[3]
+    position = fragments[4]
+
+    fragments = msg.split(";") 
+    player = {
+      name: fragments.first,
+      avatar: avatar(fragments.last)
+    }
+    puts "sending event: #{player}"
+    send_event("kicker-player-#{color}-#{position}", { player: player })
+  end
+end
+
+def subscribe(pattern)
   Thread.new do
     begin
       redis = Redis.new
-      redis.psubscribe("kicker:register:*") do |on|
+      redis.psubscribe(pattern) do |on|
         on.pmessage do |channel, msg|
-          fragments = channel.split(":")
-          color = fragments[3]
-          position = fragments[4]
-
-          fragments = msg.split(";") 
-          player = {
-            name: fragments.first,
-            avatar: avatar(fragments.last)
-          }
-          puts "sending event: #{player}"
-          send_event("kicker-player-#{color}-#{position}", { player: player })
+          yield channel, msg
         end
       end
     rescue Exception => e
@@ -39,4 +56,5 @@ def wait_for_players
   end
 end
 
-wait_for_players()
+wait_for_players
+#wait_for_score
